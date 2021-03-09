9935336035e3a8b35281b85d6d07d6c038dc0368
Tom <tom.milton91@gmail.com>
2021-03-08 21:38:15 -0500

    Added random functionality, Favourite (no functionality) and changed the next and previous buttons to icons

--- src/App.js
+++ src/App.js
@@ -17,33 +17,56 @@ comicApp.baseUrl = 'https://xkcd.com/info.0.json';
 
 //HELP QUE NUM 2
 //targeting the text area using query selector returns NULL after the inital load
+  // not the query selector but 
 comicApp.getDefault = () =>{
 
-  const apiUrl = 'https://xkcd.com/info.0.json';
+  //Proxied the API to avoid a CORS error from crashing the site (Thanks Susan!)
+  const proxiedURL = 'https://xkcd.com/info.0.json';
+ 
+  const url = new URL('http://proxy.hackeryou.com');
+  url.search = new URLSearchParams({
+    reqUrl: proxiedURL,
+  });
 
-  fetch(apiUrl)
+  fetch(url)
     .then((response) => {
       return response.json();
     }).then((data) => {
       console.log(data);
       comicApp.displayDefaultComic(data);
       comicApp.currentNumber = data.num;
+      comicApp.newestComic = data.num;
+
     })
 }
 
 comicApp.changeComic = () => {
   //fetch the API at the new location of index
-  fetch(`https://xkcd.com/${comicApp.currentNumber}/info.0.json`)
-    .then((response) => {
-      return response.json();
-    }).then((data) => {
 
+  const proxiedURL = `https://xkcd.com/${comicApp.currentNumber}/info.0.json`;
 
-      console.log(data.num);
-      comicApp.displayDefaultComic(data);
-      comicApp.currentNumber = data.num;
-    })
+  const url = new URL('http://proxy.hackeryou.com');
+  url.search = new URLSearchParams({
+    reqUrl: proxiedURL,
+  });
 
+  //TRY/Catch Does not work!! 
+  try {
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      }).then((data) => {
+  
+  
+        console.log(data.num);
+        comicApp.displayDefaultComic(data);
+        comicApp.currentNumber = data.num;
+      })
+  }
+  catch(err){
+    alert("The page does not exsist!");
+  }
 
 }
 
@@ -71,44 +94,50 @@ comicApp.displayDefaultComic = (data) => {
   currImg.src = currComic;
   currImg.alt = comicAlt;
 
+  mainEl.appendChild(currImg);
   // VVV Currently doesnt work, tells me that currNum.innerText is null after emptying the main elementnst currNum = document.querySelector("textarea");
     //Tried to switch the type of element to a div instead of a textarea
 
-  // const currNum = document.querySelector("textarea");
-  // if(currNum){
-  //   console.log("Works!");
-  // }
-  // else{
-  //   console.log("Nope!");
-  // }
-
-  // currNum.innerText = comicNum;
-
-  // console.log(currNum);
-  // mainEl.appendChild(currNum);
-
-
-  mainEl.appendChild(currImg);
+  const currNum = document.querySelector("textarea");
+  if(currNum){
+    console.log("Works!");
+    currNum.innerText = comicNum;
+    mainEl.appendChild(currNum);
+  }
+  else{
+    console.log("Nope!");
+  }
 }
 
 // EVENT HANDLERS
-// the next and previous buttons need the data passed in or at the very least the comics current number
-//The next button handler 
-const handleNext = function () {
-  //steps to run when event is "heard"
-  comicApp.currentNumber = comicApp.currentNumber + 1;
-  comicApp.changeComic();
-
-}
+//change the global variable up or down 1 depending on the button pushed 
 //The previous button handler 
 const handlePrevious = function () {
-  //steps to run when event is "heard"
   //change the currentNumber value to be one lower than the previous call
   comicApp.currentNumber = comicApp.currentNumber -1;
   comicApp.changeComic();
 
- 
+  
 }
+
+//The next button handler 
+const handleNext = function () {
+  comicApp.currentNumber = comicApp.currentNumber + 1;
+  comicApp.changeComic();
+
+}
+//the random button handler
+  //Uses a simple Math library call to get a random comic from the API
+const handleRandom = function () {
+  comicApp.currentNumber = Math.floor(Math.random() * comicApp.newestComic);
+  comicApp.changeComic();
+}
+
+//Will call a firebase API to add the current comic to  Firebase
+const handleFavourite = function () {
+  console.log("Fav has been click");  
+}
+
 // initialize the comicApp
 comicApp.init = () => {
   //current number initialized to be replaced with the first API call (Maybe not needed)
@@ -116,18 +145,23 @@ comicApp.init = () => {
   comicApp.getDefault();
 }
 
+
 function App() {
   return (
     <div className="App">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+
       <h1>Comic Viewer</h1>
       <h2></h2>
       <main></main>
 
       {/* Area for the comic number to appear */}
       <section>
-        <button onClick={handlePrevious}>Previous</button>
+        <i className="fa fa-arrow-left" onClick={handlePrevious}></i>
+        <i className="fa fa-question" onClick={handleRandom}></i>
         <textarea></textarea>
-        <button onClick={handleNext} >Next</button>
+        <i className="fa fa-star" onClick={handleFavourite}></i>
+        <i className="fa fa-arrow-right" onClick={handleNext}></i>
       </section>
       <footer>
         <a href="https://xkcd.com/license.html">Copyright Info </a>
