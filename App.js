f52022b3cd1371bbfb8d00aa037a43e0ec949a72
awd911 <36082238+awd911@users.noreply.github.com>
2023-03-30 14:24:23 -0400

    Updated App,js for the Main directory

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import './App.css';
 
+let mostRecentComic = 2500;
+
 // creating namespace
 const comicApp = {};
 
@@ -12,12 +14,14 @@ comicApp.baseUrl = 'http://localhost:8010/proxy/info.0.json';
 
 comicApp.getDefault = () =>{
 
-  const axios = require('axios').default;
-
+ // const axios = require('axios').default;
+  
   const apiUrl = 'https://xkcd.com/info.0.json';
+  const url = "https://proxy.junocollege.com/https://xkcd.com/info.0.json";
+
      axios({
          method:'GET',
-         url: 'https://proxy.junocollege.com/https://xkcd.com/info.0.json',
+          url: url,
          responseType:'json',
          params:{
              reqUrl:apiUrl,
@@ -28,35 +32,36 @@ comicApp.getDefault = () =>{
          }
      })
      .then((response) => {
-        console.log(response.data);
-        comicApp.displayDefaultComic(response.data);
+      mostRecentComic = response.data.num;
+        console.log("!!!!! Axios Initial : ",response.data, "Last Comic ! ",mostRecentComic);
+        // comicApp.displayDefaultComic(response.data);
+        // comicApp.currentNumber = response.data.num;
      })
 
 
-//   fetch(apiUrl)
-//     .then((response) => {
-//       return response.json();
-//     }).then((data) => {
-//       console.log(data);
-//       comicApp.displayDefaultComic(data);
-//       comicApp.currentNumber = data.num;
-//     })
-}
+  fetch(url)
+    .then((response) => {
+     return response.json();
+    }).then((data) => {
+      comicApp.displayDefaultComic(data);
+      comicApp.currentNumber = data.num;
+    })
+ }
 
 comicApp.changeComic = () => {
   //fetch the API at the new location of index
-  console.log("not yet...");
-//   fetch(`https://xkcd.com/${comicApp.currentNumber}/info.0.json`)
+  //console.log("not yet...");
+  fetch(`https://proxy.junocollege.com/https://xkcd.com/${comicApp.comicNum}/info.0.json`)
   
-//     .then((response) => {
-//       return response.json();
-//     }).then((data) => {
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
 
-
-//       console.log(data.num);
-//       comicApp.displayDefaultComic(data);
-//       comicApp.currentNumber = data.num;
-//     })
+      console.log("Current Num : ",comicApp.currNumber, " data.num: ",data.num);
+      comicApp.displayDefaultComic(data);
+      
+      //currentNumber = data.num;
+    })
 
 
 }
@@ -67,7 +72,11 @@ comicApp.displayDefaultComic = (data) => {
   const comicTitle = data.title;
   const currComic = data.img;
   const comicAlt = data.alt;
- // const comicNum = data.num;
+  const comicNum = data.num;
+  const currentNumber = data.num;
+
+  console.log("!! displayDefault Title",comicTitle," comicNum + currentNumber = ",comicNum, currentNumber);
+  //currentNumber = data.num;
 
   
 
@@ -85,21 +94,40 @@ comicApp.displayDefaultComic = (data) => {
   currImg.src = currComic;
   currImg.alt = comicAlt;
 
-  // VVV Currently doesnt work, tells me that currNum.innerText is null after emptying the main elementnst currNum = document.querySelector("textarea");
-    //Tried to switch the type of element to a div instead of a textarea
 
-  // const currNum = document.querySelector("textarea");
-  // if(currNum){
-  //   console.log("Works!");
+
+  // Fills the text area with the current Comic Number, if there isnt a text area, creates one first
+
+  let currTextArea = document.querySelector("textarea");
+  if(currTextArea){
+    currTextArea.innerText = comicNum;
+    mainEl.appendChild(currTextArea);
+  }
+  else{
+    let newTextArea = document.createElement("textarea");
+    newTextArea.id = "currentIssue";
+    let textAreaNumber = document.createTextNode(comicNum);
+    newTextArea.append(textAreaNumber);
+
+    
+    //currTextArea.innerHTML = ;
+    mainEl.appendChild(newTextArea)
+    document.getElementById("currentIssue").disabled = true;
+  
+    console.log("Nope! Text area does not exist!");
+  }
+
+
+  //console.log("CurrNum: ",comicApp.comicNum, " > ",mostRecentComic);
+
+  // if(comicApp.comicNum > mostRecentComic){
+  //   document.getElementById("NextButton").disabled = true;
+  //   
   // }
   // else{
-  //   console.log("Nope!");
+  //   document.getElementById("NextButton").disabled = false;
   // }
 
-  // currNum.innerText = comicNum;
-
-  // console.log(currNum);
-  // mainEl.appendChild(currNum);
 
 
   mainEl.appendChild(currImg);
@@ -110,23 +138,106 @@ comicApp.displayDefaultComic = (data) => {
 //The next button handler 
 const handleNext = function () {
   //steps to run when event is "heard"
-  comicApp.currentNumber = comicApp.currentNumber + 1;
-  comicApp.changeComic();
+  comicApp.comicNum = comicApp.currentNumber + 1;
+  document.getElementById("PreviousButton").disabled = false;
+
+  if (comicApp.comicNum> mostRecentComic){
+    document.getElementById("NextButton").disabled = true;
+    console.log("~~~~~~~~~~~~~~~~ Max level reached ! ! ! ~~~~~~~~~~~~~~~");
+  }
+  else{
+    comicApp.currentNumber++;
+    console.log("ComicApp MAX: ", mostRecentComic, " ComicNum : ",comicApp.comicNum);
+    comicApp.changeComic();
+  }
+
+  
 
 }
 //The previous button handler 
 const handlePrevious = function () {
   //steps to run when event is "heard"
   //change the currentNumber value to be one lower than the previous call
-  comicApp.currentNumber = comicApp.currentNumber -1;
+  
+  if (comicApp.comicNum >= mostRecentComic){
+    document.getElementById("NextButton").disabled = false;
+    console.log("~~~~~~~~~~~~~~~~ Descending from max level ~~~~~~~~~~~~~~~");
+  }
+
+  //Makes sure the comic cant go below 1
+  if (comicApp.comicNum === 1){
+    document.getElementById("PreviousButton").disabled = true;
+  }
+  else{
+    comicApp.comicNum = comicApp.currentNumber -1;
+    comicApp.currentNumber--;
+    console.log("ComicApp Current number DOWN: ", comicApp.currentNumber, " ComicNum : ",comicApp.comicNum);
+    comicApp.changeComic();
+
+  }
+
+ 
+}
+//Sends the user to the End of the API
+const handleLast = function () {
+  //steps to run when event is "heard"
+  //change the currentNumber value to be one
+  comicApp.currentNumber = mostRecentComic;
+  comicApp.comicNum = comicApp.currentNumber;
+  
+  document.getElementById("NextButton").disabled = true;
+  document.getElementById("PreviousButton").disabled = false;
+  console.log("ComicApp Current number DOWN: ", comicApp.currentNumber, " ComicNum : ",comicApp.comicNum);
+  comicApp.changeComic();
+
+ 
+}
+
+//Sends the user to the beginning of the API
+const handleFirst = function () {
+  //steps to run when event is "heard"
+  //change the currentNumber value to be one
+  comicApp.currentNumber = 1;
+  comicApp.comicNum = comicApp.currentNumber;
+  
+  if (comicApp.comicNum === 1){
+    document.getElementById("PreviousButton").disabled = true;
+    document.getElementById("NextButton").disabled = false;
+  }
+  
+  console.log("ComicApp Current number DOWN: ", comicApp.currentNumber, " ComicNum : ",comicApp.comicNum);
+  comicApp.changeComic();
+
+ 
+}
+
+//Sends the user to a random comic in the API
+const handleRandom = function () {
+  //steps to run when event is "heard"
+  //change the currentNumber value to be one
+  comicApp.currentNumber = Math.floor(Math.random() * (mostRecentComic - 1 + 1) + 1);
+  comicApp.comicNum = comicApp.currentNumber;
+  
+
+  if (comicApp.currentNumber === mostRecentComic){
+    document.getElementById("PreviousButton").disabled = false;
+    document.getElementById("NextButton").disabled = true;
+  }
+  if (comicApp.currentNumber === 1){
+    document.getElementById("PreviousButton").disabled = true;
+    document.getElementById("NextButton").disabled = false;
+  }
+  
+  console.log("ComicApp Current number RANDO: ", comicApp.currentNumber, " ComicNum : ",comicApp.comicNum);
   comicApp.changeComic();
 
  
 }
 // initialize the comicApp
+
 comicApp.init = () => {
   //current number initialized to be replaced with the first API call (Maybe not needed)
-  comicApp.currentNumber = 0;
+  //comicApp.currentNumber = 0;
   comicApp.getDefault();
 }
 
@@ -134,14 +245,17 @@ function App() {
   return (
     <div className="App">
       <h1>Comic Viewer</h1>
-      <h2></h2>
+      <h2>XKCD : </h2>
+      <textarea disabled></textarea>
       <main></main>
 
       {/* Area for the comic number to appear */}
       <section>
-        <button onClick={handlePrevious}>Previous</button>
-        <textarea></textarea>
-        <button onClick={handleNext} >Next</button>
+        <button id = "FirstButton" onClick={handleFirst}>First</button>
+        <button id ="PreviousButton" onClick={handlePrevious}>Previous</button>
+        <button id ="RandomButton" onClick={handleRandom}>Random</button>
+        <button id ="NextButton" onClick={handleNext} >Next</button>
+        <button id ="LastButton" onClick={handleLast} >Last</button>
       </section>
       <footer>
         <a href="https://xkcd.com/license.html">Copyright Info </a>
